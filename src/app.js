2181ccca92e255d830c8f132fd183d8833ad161d
negiamishhu <negiamishu@gmail.com>
2025-10-27 19:34:32 +0530

    APP.JS CHNGED

--- src/app.js
+++ src/app.js
@@ -17,8 +17,7 @@ const allowedOrigins = [
 // Middleware
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+     if (!origin) return callback(null, true);
     
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
